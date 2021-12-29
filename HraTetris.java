b0715c0b3d1b29153b96535ee988cd719c2595b0
n0rthking <93261911+n0rthking@users.noreply.github.com>
2021-12-29 13:30:17 +0100

    add menu to restart and quit game

--- HraTetris.java
+++ HraTetris.java
@@ -1,6 +1,7 @@
 package fun.n0rthking;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class HraTetris {
     private Mriezka mriezka;
@@ -10,6 +11,8 @@ public class HraTetris {
     private int pocet;
     private boolean koniecHry;
     private int skore;
+    private boolean pauza;
+    private int rychlost;
 
     public HraTetris() {
         this.mriezka = new Mriezka(20, 10);
@@ -19,6 +22,8 @@ public class HraTetris {
         this.pocet = 0;
         this.koniecHry = false;
         this.skore = 0;
+        this.pauza = false;
+        this.rychlost = 4;
     }
 
     public void start() {
@@ -26,10 +31,7 @@ public class HraTetris {
         this.aktualnyTvar = new Tvar(this.mriezka, this.generator, 0, 4);
     }
 
-    public void zrus() {
-        if (!this.koniecHry) {
-            return;
-        }
+    private void restartujHru() {
         for (int riadok = 0; riadok < this.mriezka.getVyska(); riadok++) {
             for (int stlpec = 0; stlpec < this.mriezka.getSirka(); stlpec++) {
                 if (!this.mriezka.getBunka(riadok, stlpec).getFarba().equals("black")) {
@@ -42,62 +44,101 @@ public class HraTetris {
         this.aktualnyTvar = new Tvar(this.mriezka, this.generator, 0, 4);
     }
 
+    public void aktivuj() {
+        this.pauza = !this.pauza;
+    }
+
     private int vypocitajSkore(int pocetOdstranenych) {
         switch (pocetOdstranenych) {
             case 1:
-                return 40;
-            case 2:
                 return 100;
+            case 2:
+                return 200;
             case 3:
-                return 300;
+                return 400;
             case 4:
-                return 1200;
+                return 800;
         }
         return 0;
     }
 
+    private void vypisMoznosti() {
+        System.out.println("vyber si moznost:");
+        System.out.println("[n]ova hra");
+        System.out.println("[u]koncit program");
+
+        Scanner nacitavac = new Scanner(System.in);
+        boolean nacitavanieUkoncene = false;
+
+        while (!nacitavanieUkoncene) {
+            switch (nacitavac.next().toLowerCase()) {
+                case "n":
+                    System.out.println("bola spustena nova hra");
+                    this.restartujHru();
+                    nacitavanieUkoncene = true;
+                    break;
+                case "u":
+                    System.out.println("program ukonceny");
+                    this.manazer.prestanSpravovatObjekt(this);
+                    nacitavanieUkoncene = true;
+                    break;
+                default:
+                    System.out.println("neznama moznost, skus to znova:");
+                    System.out.println("[n]ova hra");
+                    System.out.println("[u]koncit program");
+                    break;
+            }
+        }
+    }
+
     public void tik() {
-        if (this.koniecHry) {
+        if (this.koniecHry || this.pauza) {
             return;
         }
         this.pocet++;
-        if (this.pocet == 5) {
+        if (this.pocet >= this.rychlost) {
             this.pocet = 0;
             if (!this.aktualnyTvar.posunTvar(1, 0)) {
+                this.rychlost = 4;
                 this.skore += this.vypocitajSkore(this.mriezka.odstranZaplneneRiadky());
                 this.aktualnyTvar = new Tvar(this.mriezka, this.generator, 0, 4);
                 if (!this.aktualnyTvar.getStav()) {
                     this.koniecHry = true;
                     System.out.println("koniec hry");
                     System.out.println("skore " + this.skore);
+                    this.vypisMoznosti();
                 }
             }
         }
     }
 
     public void posunHore() {
-        if (this.koniecHry) {
+        if (this.koniecHry || this.pauza) {
             return;
         }
         this.aktualnyTvar.otocTvar();
     }
 
     public void posunDole() {
-        if (this.koniecHry) {
+        if (this.koniecHry || this.pauza) {
             return;
         }
-        this.aktualnyTvar.posunTvar(1, 0);
+        if (this.rychlost == 4) {
+            this.rychlost = 1;
+        } else {
+            this.rychlost = 4;
+        }
     }
 
     public void posunVlavo() {
-        if (this.koniecHry) {
+        if (this.koniecHry || this.pauza) {
             return;
         }
         this.aktualnyTvar.posunTvar(0, -1);
     }
 
     public void posunVpravo() {
-        if (this.koniecHry) {
+        if (this.koniecHry || this.pauza) {
             return;
         }
         this.aktualnyTvar.posunTvar(0, 1);
